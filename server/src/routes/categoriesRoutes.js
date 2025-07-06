dbae73a08fdb502f4292960e901eed9073285e30
Md Kutub Islam <mdkutubislam09@gmail.com>
2025-07-06 16:57:30 +0530

    Refactor category controller: enhance error handling, improve response structure, and ensure user validation for category operations (create, read, update, delete).

--- server/src/routes/categoriesRoutes.js
+++ server/src/routes/categoriesRoutes.js
@@ -6,8 +6,8 @@ const router = Router();
 
 router.route("/create-category").post(authMiddleware, createCategory);
 router.route("/get-categories").get(authMiddleware, getCategories);
-router.route("/get-category/:id").get(authMiddleware, getOneCategory);
-router.route("/update-category/:id").put(authMiddleware, updateCategory);
-router.route("/delete-category/:id").delete(authMiddleware, deleteCategory);
+router.route("/get-category/:categoryId").get(authMiddleware, getOneCategory);
+router.route("/update-category/:categoryId").put(authMiddleware, updateCategory);
+router.route("/delete-category/:categoryId").delete(authMiddleware, deleteCategory);
 
 export default router;
